c1fc835790ed2a17429deb15cf5e6a6cfd15d801
Dani <thedanikun@gmail.com>
2015-07-13 20:22:53 +0200

    Primera pregunta
    Primera pregunta

---
+++ quiz/controllers/quiz_controller.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//GET /quizes/question
+exports.question = function(req, res){
+    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
+
+//GET /quizes/answer
+exports.answer = function(req, res){
+    if( req.query.respuesta === "Roma" ){
+        res.render('quizes/answer', {respuesta: 'Correcto'});
+    } 
+    else {
+        res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    }
+};
