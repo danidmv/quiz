0866b8937669a742cba992920e26c6ac83e4828d
Dani <thedanikun@gmail.com>
2015-07-13 19:29:48 +0200

    debug app

--- quiz/app.js
+++ quiz/app.js
@@ -1,35 +1,37 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express = require('express');       //importar
+var path = require('path');             //path
+var favicon = require('serve-favicon'); //favicon
+var logger = require('morgan');         //middlewares
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//importar enrutadores
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = express();    //crear aplicación
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); //instalar generador de vistas ejs
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json());     //instalar middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//asociar rutas a sus gestores
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Not Found');   //resto de rutas
     err.status = 404;
-    next(err);
+    next(err);                          //genera error 404 de http
 });
 
 // error handlers
@@ -38,7 +40,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
+        res.status(err.status || 500);  //gestión de errores durante el desarrollo
         res.render('error', {
             message: err.message,
             error: err
@@ -49,12 +51,12 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500);      //gestión de errores de producción
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
 
-
+//exportar app para comando de arranque
 module.exports = app;
